bf92b19ae97632ca0a8f3038bd858c24d84b6a0e
olufeoluwa <odebunmioluwadamilola3@gmail.com>
2025-06-13 13:53:11 +0100

    my first commit

---
+++ Sugar.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as pt
+import numpy as np
+import seaborn as sns
+
+#import my csv file
+df = pd.read_csv("diabetes.csv")
+st.write(df.head())
+
