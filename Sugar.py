83e75c572b0bb422eb64c69b34440acf844dfe96
olufeoluwa <odebunmioluwadamilola3@gmail.com>
2025-06-13 14:48:25 +0100

    Update Sugar.py

--- Sugar.py
+++ Sugar.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as pt
 import numpy as np
 import seaborn as sns
 
+st.title("DIABETES ANALYSIS")
 #import my csv file
 df = pd.read_csv("diabetes.csv")
-st.title("DIABETES DATA")
 st.write(df.head())
 
