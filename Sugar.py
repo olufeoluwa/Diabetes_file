6931604901742727e7d1214815cd37915da9e129
olufeoluwa <odebunmioluwadamilola3@gmail.com>
2025-06-16 14:22:01 +0100

    Update Sugar.py

--- Sugar.py
+++ Sugar.py
@@ -3,6 +3,7 @@ import streamlit as st
 import matplotlib.pyplot as pt
 import numpy as np
 import seaborn as sns
+import plotly.express as px
 
 st.title("DIABETES ANALYSIS")
 
@@ -40,3 +41,18 @@ st.write(df["BloodPressure"].head())
 
 st.markdown("## Blood Pressure Last Data")
 st.write(df["BloodPressure"].tail())
+
+st.markdown("## Blood Pressure Graph")
+fig = px.bar(df['BloodPressure'], y ='BloodPressure', title=("Blood Pressure Distribution Graph"))
+st.plotly_chart(fig, use_containeer_width=True)
+
+
+
+st.markdown("# BIVAFIATE ANALYSIS")
+
+st.markdown("## Blood Pressure vs Pregnancies Description")
+
+fig2 = px.bar(df, x ='Pregnancies', y ='BloodPressure', title=("Blood Pressure vs Pregnancies Distribution Graph"))
+st.plotly_chart(fig2, use_containeer_width=True)
+
+
